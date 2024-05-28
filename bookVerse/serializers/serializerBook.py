5d1433f7b3f79e614b70919d9215c9b8db5f9414
Luana Seres Girotto <123522626+LuanaSeres@users.noreply.github.com>
2024-05-27 22:22:56 -0300

    chore: reorganizando as pastas para melhor desenvolvimento do codigo e atualizando algumas importações

--- bookVerse/serializers/serializerBook.py
+++ bookVerse/serializers/serializerBook.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book
+from ..models.modelBook import Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
--- bookVerse/views/viewBook.py
+++ bookVerse/views/viewBook.py
@@ -3,9 +3,9 @@ from django.http import JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from .models import Book
-from .serializers import BookSerializer
-from .repository import BookRepository
+from ..models.modelBook import Book
+from ..serializers.serializerBook import BookSerializer
+from ..repository import BookRepository
 
 @method_decorator(csrf_exempt, name='dispatch')
 class BookListView(View):
