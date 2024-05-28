1e86b62c53f4d522a5eb66c3577c25d659eb729a
Talia-goncalves <gtalia246@gmail.com>
2024-05-27 21:44:35 -0300

    feat: criando repository Book da aplicacao

---
+++ bookVerse/repository.py
@@ -0,0 +1,24 @@
+from .models import Book
+
+class BookRepository:
+    def get_all_books():
+        return Book.objects.all()
+
+    def get_book_by_id(book_id):
+        try:
+            return Book.objects.get(pk=book_id)
+        except Book.DoesNotExist:
+            return None
+
+    def create_book(data):
+        book = Book.objects.create(**data)
+        return book
+
+    def update_book(book, data):
+        for field, value in data.items():
+            setattr(book, field, value)
+        book.save()
+        return book
+
+    def delete_book(book):
+        book.delete()
