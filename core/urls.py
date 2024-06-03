6321e5be1539daa887052f9bb401859f558f9b6e
Talia-goncalves <gtalia246@gmail.com>
2024-06-03 20:50:49 -0300

    fix: corrigindo as urls do Review e do Book

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
 from django.urls import path
-from bookVerse.views.viewBook import BookListView, BookDetailView
-from bookVerse.views.viewReview import ReviewListView, ReviewDetailView
+from bookVerse.views.viewBook import *
+from bookVerse.views.viewReview import *
 from bookVerse.views.viewWishList import WishListView, WishlistDetailView
 from bookVerse.views.viewReadingStatus import ReadingStatusView, ReadingStatusDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/books/', BookListView.as_view(), name='book-list-create'),
-    path('api/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('api/comments/', ReviewListView.as_view(), name='review-list-create'),
-    path('api/comments/<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('reviews/', ReviewListView.as_view(), name='review-list'),
+    path('reviews/create/', ReviewCreateView.as_view(), name='review-create'),
+    path('reviews/<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
+    path('reviews/<int:pk>/update/', ReviewUpdateView.as_view(), name='review-update'),
+    path('reviews/<int:pk>/delete/', ReviewDeleteView.as_view(), name='review-delete'),
     path('api/wishlist/', WishListView.as_view(), name='wishlist'),
     path('api/wishlist/<int:pk>/', WishlistDetailView.as_view(), name='wishlist-detail'),
     path('api/reading-status/', ReadingStatusView.as_view(), name='reading-status'),
