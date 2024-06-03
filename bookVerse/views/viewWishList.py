7d352e25c0093a702d71fc5fd6d4a7d31dd530c6
Talia-goncalves <gtalia246@gmail.com>
2024-06-03 14:58:30 -0300

    feat: criando a view da WishList

---
+++ bookVerse/views/viewWishList.py
@@ -0,0 +1,45 @@
+import json
+from django.http import JsonResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from ..models.modeWishList import Wishlist
+from ..serializers.serializerWishList import WishlistSerializer
+from ..repository import WishlistRepository
+
+@method_decorator(csrf_exempt, name='dispatch')
+class WishlistListView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        serializer = WishlistSerializer(data=data)
+        if serializer.is_valid():
+            wishlist = WishlistRepository.create_wishlist(serializer.validated_data)
+            return JsonResponse(WishlistSerializer(wishlist).data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class WishlistDetailView(View):
+    def get(self, request, user_id):
+        wishlist = WishlistRepository.get_wishlist_by_user(user_id)
+        if wishlist is None:
+            return JsonResponse({'error': 'Wishlist not found'}, status=404)
+        serializer = WishlistSerializer(wishlist)
+        return JsonResponse(serializer.data)
+
+    def put(self, request, user_id):
+        wishlist = WishlistRepository.get_wishlist_by_user(user_id)
+        if wishlist is None:
+            return JsonResponse({'error': 'Wishlist not found'}, status=404)
+        data = json.loads(request.body)
+        serializer = WishlistSerializer(wishlist, data=data)
+        if serializer.is_valid():
+            wishlist = WishlistRepository.update_wishlist(wishlist, serializer.validated_data)
+            return JsonResponse(WishlistSerializer(wishlist).data)
+        return JsonResponse(serializer.errors, status=400)
+
+    def delete(self, request, user_id):
+        wishlist = WishlistRepository.get_wishlist_by_user(user_id)
+        if wishlist is None:
+            return JsonResponse({'error': 'Wishlist not found'}, status=404)
+        WishlistRepository.delete_wishlist(wishlist)
+        return JsonResponse({'message': 'Wishlist deleted successfully'}, status=204)
