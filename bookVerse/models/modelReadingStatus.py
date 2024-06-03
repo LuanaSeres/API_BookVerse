c0b0ce42b49e779ba3cd983506529172d3679c38
Luana Seres Girotto <123522626+LuanaSeres@users.noreply.github.com>
2024-06-03 19:05:39 -0300

    feat: criado o model do ReadingStatus

---
+++ bookVerse/models/modelReadingStatus.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import models
+from ..models.modelBook import Book
+
+class ReadingStatus(models.Model):
+    STATUS_CHOICES = [
+        ('read', 'Read'),
+        ('unread', 'Unread'),
+        ('reading', 'Reading'),
+    ]
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reading_statuses')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    def _str_(self):
+        return f'{self.user.username} - {self.book.title} - {self.status}'
