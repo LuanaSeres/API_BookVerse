30ca02a236e528fbb3bb6b704ba376cd84261817
Talia-goncalves <gtalia246@gmail.com>
2024-06-03 21:51:26 -0300

    fix: adicionando progresso de leitura em Reading Status

--- bookVerse/models/modelReadingStatus.py
+++ bookVerse/models/modelReadingStatus.py
@@ -11,6 +11,7 @@ class ReadingStatus(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reading_statuses')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    progress = models.IntegerField(default=0)
 
     def _str_(self):
         return f'{self.user.username} - {self.book.title} - {self.status}'
