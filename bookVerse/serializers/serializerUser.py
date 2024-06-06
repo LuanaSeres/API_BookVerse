153f13b326ae161d6e10ad4d4a1265bb2a5cbccc
Talia-goncalves <gtalia246@gmail.com>
2024-06-06 12:32:26 -0300

    doc: adicionando comentários para melhor entendimento do serializerUser

--- bookVerse/serializers/serializerUser.py
+++ bookVerse/serializers/serializerUser.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+# Serializer para o modelo User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        model = User  
+        fields = ('id', 'username', 'email', 'password')  # Inclui apenas os campos id, username, email e password no serializer
+        extra_kwargs = {'password': {'write_only': True}}  # Define que o campo password é apenas para escrita, não será retornado na resposta
 
+    # Método para criar um novo usuário
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+        user = User.objects.create_user(**validated_data)  # Cria um novo usuário usando o método create_user, que lida com a hash da senha
+        return user  # Retorna o usuário criado
