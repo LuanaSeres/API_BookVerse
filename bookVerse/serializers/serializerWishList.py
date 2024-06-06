48d638cdf50f32016b085d7ff7cceef365bd22e1
Talia-goncalves <gtalia246@gmail.com>
2024-06-06 12:32:43 -0300

    doc: adicionando comentários para melhor entendimento do serializerWishList

--- bookVerse/serializers/serializerWishList.py
+++ bookVerse/serializers/serializerWishList.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models.modelWishList import Wishlist
 
+# Serializer para o modelo Wishlist incluindo todos os campos do mesmo.
 class WishlistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wishlist
