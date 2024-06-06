434d200e07f8d7c1e95e15b759cbe38c2f48ca46
Talia-goncalves <gtalia246@gmail.com>
2024-06-06 12:30:46 -0300

    doc: adicionando comentários para melhor entendimento do forms

--- bookVerse/forms.py
+++ bookVerse/forms.py
@@ -5,35 +5,39 @@ from .models.modelWishList import Wishlist
 from django.contrib.auth.models import User
 from .models.modelReadingStatus import ReadingStatus
 
+# Formulário para o modelo Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'genre', 'description']
+        fields = ['title', 'author', 'genre', 'description']  # Campos do modelo Book que serão incluídos no formulário
 
+# Formulário para o modelo Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['rating', 'comment'] 
+        fields = ['rating', 'comment']  # Campos do modelo Review que serão incluídos no formulário
 
-
+# Formulário para o modelo Wishlist
 class WishlistForm(forms.ModelForm):
     class Meta:
         model = Wishlist
-        fields = ['title', 'author', 'genre', 'description', 'priority']
+        fields = ['title', 'author', 'genre', 'description', 'priority']  # Campos do modelo Wishlist que serão incluídos no formulário
 
+# Formulário para o modelo ReadingStatus
 class ReadingStatusForm(forms.ModelForm):
     class Meta:
-        model = ReadingStatus
-        fields = ['status', 'progress'] 
+        model = ReadingStatus 
+        fields = ['status', 'progress']  # Campos do modelo ReadingStatus que serão incluídos no formulário
 
+# Formulário para o modelo User
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)  # Campo de senha, usando widget PasswordInput para ocultar a entrada
 
     class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
+        model = User  
+        fields = ['username', 'email', 'password']  # Campos do modelo User que serão incluídos no formulário
 
+# Formulário para login de usuário
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
-
+    username = forms.CharField(max_length=150) 
+    password = forms.CharField(widget=forms.PasswordInput)  # Campo de senha, usando widget PasswordInput para ocultar a entrada
