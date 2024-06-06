8b377810f497cabe01b20e6f0ed6671ece4faa20
Talia-goncalves <gtalia246@gmail.com>
2024-06-06 12:31:35 -0300

    doc: adicionando comentários para melhor entendimento do serializerReadingStatus

--- bookVerse/serializers/serializerReadingStatus.py
+++ bookVerse/serializers/serializerReadingStatus.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from ..models.modelReadingStatus import ReadingStatus
 
+# Serializer para o modelo ReadingStatus incluindo todos os campos do mesmo.
 class ReadingStatusSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ReadingStatus
-        fields = '_all_'
+        model = ReadingStatus  
+        fields = '__all__'  
