de1c08331ead4d65fcba365a00743700d008c47c
Talia-goncalves <gtalia246@gmail.com>
2024-06-06 12:32:16 -0300

    doc: adicionando comentários para melhor entendimento do serializerReview

--- bookVerse/serializers/serializerReview.py
+++ bookVerse/serializers/serializerReview.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models.modelReview import Review
 
+# Serializer para o modelo Review incluindo todos os campos do mesmo.
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
