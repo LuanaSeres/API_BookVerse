be2e52d8c4594ff8554c01ee40a1310e5a925498
LuanaSeres <luana.sgirotto@gmail.com>
2024-06-16 12:57:46 -0300

    doc: adicionando comentários para melhor entendimento do model wish list

--- bookVerse/models/modelWishList.py
+++ bookVerse/models/modelWishList.py
@@ -2,6 +2,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
 
+# Define um novo modelo chamado Wishlist (Lista de Desejos)
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
@@ -10,5 +11,6 @@ class Wishlist(models.Model):
     description = models.TextField()
     priority = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(10)])
 
-    def _str_(self):
+    # Define o método especial __str__ que retorna o título do livro quando o objeto é representado como string
+    def __str__(self):
         return self.title
